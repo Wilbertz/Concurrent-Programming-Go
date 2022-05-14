66961bb560a43c7974854d1284605fc370ed11e4
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-14 21:31:37 +0200

    Chanel types used.

--- Channel/main.go
+++ Channel/main.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan int, 1)
 
 	wg.Add(2)
 
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
 		wg.Done()
 	}(ch, wg)
