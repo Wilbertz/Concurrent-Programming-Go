bf0e012b13be059e6317e97db7272696e174a334
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-14 20:34:29 +0200

    Mutex added.

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ func main() {
 		wg.Add(2)
 		id := rnd.Intn(10) + 1
 		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
-			if b, ok := queryCache(id); ok {
+			if b, ok := queryCache(id, m); ok {
 				fmt.Println("from cache", b)
 			}
 			wg.Done()
 		}(id, wg, m)
 
 		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
-			if b, ok := queryDatabase(id); ok {
+			if b, ok := queryDatabase(id, m); ok {
 				fmt.Println("from database", b)
 			}
 			wg.Done()
@@ -35,16 +35,20 @@ func main() {
 	wg.Wait()
 }
 
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, m *sync.Mutex) (Book, bool) {
+	m.Lock()
 	b, ok := cache[id]
+	m.Unlock()
 	return b, ok
 }
 
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, m *sync.Mutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
